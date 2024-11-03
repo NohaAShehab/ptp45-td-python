3f52acfa7966f618e5e44af3d5c076a2160c0897
NohaShehab <nohashehab.iti@gmail.com>
2024-11-03 12:01:24 +0200

    working with strings

--- playground.py
+++ playground.py
@@ -1,10 +1,62 @@
 
 
-num = input("Enter a number : ")
+# num = input("Enter a number : ")
+#
+#
+# # validate if input valid number or not ??
+#
+# num = int(num)
+#
+# print(num, type(num))
 
 
-# validate if input valid number or not ??
+##########################3 debugging
 
-num = int(num)
 
-print(num, type(num))
+# num1 = input("please enter your first number: ")
+# print(num1, type(num1))
+# num2 = input("please enter your second number: ") # return string
+# print(num2, type(num2))
+# #
+# if num1.isdigit():
+#     print("here in if ")
+#     num1 =int(num1) # 2  int
+# elif num2.isdigit():
+#     print("here in else ")
+#     num2 =int(num2)
+#     print(num2 * num1)
+#
+
+# print("-0----- I am here ")
+
+
+# mood = "happy"
+#
+# for mared in mood:
+#     print(mared, mared.upper())
+#
+# print("-----------------------------")
+
+
+# check this website :: https://pythontutor.com/render.html#mode=display
+
+
+
+
+dim = input("please enter dimention")
+print(dim, len(dim))
+
+if dim:
+    print("--- added")
+else:
+    print("---- still empty ")
+
+
+# https://edabit.com/challenges/python3
+
+
+
+
+
+
+
