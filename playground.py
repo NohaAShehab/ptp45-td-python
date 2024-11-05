bb2d6e853fea763d61757132ec1b71e04e92cb3b
Noha Shehab <nohashehab.iti@gmail.com>
2024-11-05 12:49:50 +0200

    loops

---
+++ playground.py
@@ -0,0 +1,40 @@
+
+##################################### while demo ##################
+# num = input('Enter a number: ')
+# while not (num.isdigit()):
+#     print("---- please enter a valid number ----")
+#     num = input('Enter a number: ')
+#
+# num = int(num)
+# print(f"num = {num}")
+
+########################### while true demo
+# while True:
+#     num = input('Enter a number: ')
+#     if num.isdigit():
+#         num = int(num)
+#         print(f"num = {num}")
+#         break
+#     print("---> You entered a wrong number, please re-enter it ---> ")
+
+
+
+
+#####################################33
+
+counter = 0
+while counter < 10:
+    counter += 1
+    if counter == 6:
+        continue
+
+    print(f"num = {counter}")
+
+
+
+
+
+
+
+
+
