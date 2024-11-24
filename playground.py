1c9862a0fce429b177ea47bc12c1401d22a63960
Noha Shehab <nohashehab.iti@gmail.com>
2024-11-24 11:57:29 +0200

    functions part02

--- playground.py
+++ playground.py
@@ -6,3 +6,12 @@ def myfunction():
 
 # when define you to call it
 myfunction()
+
+
+print("hello world ")
+
+
+mm = "fff {0}, {1}, {2}, {3}"
+print(mm.format("23","qwe", "Qe", "qeer"))
+l= []
+l.sort()
